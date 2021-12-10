2410203bdfb6ba9c2a940db6970f11514ef2a8d5
Ayush <iushjk007@gmail.com>
2021-12-10 23:08:36 +0530

    Replace slider

--- src/screens/Dashboard/Sites.js
+++ src/screens/Dashboard/Sites.js
@@ -68,36 +68,9 @@ const Sites = (props) => {
     getAsync();
   }, [status]);
 
-  const RenderComponent = ({ item }) => {
-    const grpImages = _.chunk(
-      item.site_images ? item.site_images.split(",") : [],
-      3
-    );
-    const slides = grpImages.map((grp, idx) => (
-      <View
-        key={idx}
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          width: "100%",
-        }}
-      >
-        {grp.map((img, id) => (
-          <ImageView
-            key={id}
-            source={{ uri: taskImages + img }}
-            style={{
-              height: 100,
-              width: 90,
-              marginHorizontal: 5,
-              marginVertical: 10,
-              borderRadius: 5,
-            }}
-          />
-        ))}
-      </View>
-    ));
+  console.log(list);
 
+  const RenderComponent = ({ item }) => {
     if (userType == "client") {
       return (
         <View style={{ marginVertical: 20 }}>
@@ -113,37 +86,30 @@ const Sites = (props) => {
           <Text style={{ fontSize: 26, color: "#fe5f5b" }}>
             Media Type : {item?.medium_type?.name}
           </Text>
+          {item.free_size != "" && (
+            <Text style={{ fontSize: 26, color: "#fe5f5b" }}>
+              Free Size : {item?.free_size}
+            </Text>
+          )}
+          <Text style={{ fontSize: 26, color: "#fe5f5b" }}>
+            Total Sqft : {item?.total_area_in_sqft}
+          </Text>
 
-          <View style={{ height: 120 }}>
-            <Swiper
-              style={{ flex: 1 }}
-              loadMinimal={true}
-              autoplay={false}
-              showsPagination={false}
-              showsButtons={true}
-            >
-              {slides}
-            </Swiper>
+          <View style={{ height: 120, flexDirection: "row", flexWrap: "wrap" }}>
+            {item.site_images.split(",").map((img, id) => (
+              <ImageView
+                key={id}
+                source={{ uri: taskImages + img }}
+                style={{
+                  height: 100,
+                  width: 90,
+                  marginHorizontal: 5,
+                  marginVertical: 10,
+                  borderRadius: 5,
+                }}
+              />
+            ))}
           </View>
-
-          {/* {item.site_images != "" && (
-            <FlatList
-              data={item.site_images.split(",")}
-              renderItem={({ item }) => (
-                <ImageView
-                  source={{ uri: taskImages + item }}
-                  style={{
-                    height: 100,
-                    width: 90,
-                    marginHorizontal: 5,
-                  }}
-                />
-              )}
-              keyExtractor={(_, idx) => idx + "img"}
-              horizontal
-              style={{ height: 120 }}
-            />
-          )} */}
         </View>
       );
     }
